2aed6779cf96faa9303c112080dc691ed89e2e4e
rudal <rudals9901@naver.com>
2025-01-13 21:52:35 +0900

    feat: init

---
+++ src/main/kotlin/org/jamin/jaminsso/JaminssoApplication.kt
@@ -0,0 +1,11 @@
+package org.jamin.jaminsso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaminssoApplication
+
+fun main(args: Array<String>) {
+    runApplication<JaminssoApplication>(*args)
+}
---
+++ src/test/kotlin/org/jamin/jaminsso/JaminssoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jamin.jaminsso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaminssoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
